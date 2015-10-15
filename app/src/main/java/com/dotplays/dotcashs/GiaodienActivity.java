c2fb7968472901a88c7182853279409b0a8c92eb
leconglongvu <leconglongvu@c1197d1b-f608-4e4c-8820-b872ab0a124d>
2015-10-15 15:53:42 +0000

    Failed commit: Default

--- app/src/main/java/com/dotplays/dotcashs/GiaodienActivity.java
+++ app/src/main/java/com/dotplays/dotcashs/GiaodienActivity.java
@@ -1,7 +1,7 @@
 package com.dotplays.dotcashs;
 
+import android.app.Dialog;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -11,14 +11,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import adapter.AdapterGiaodien;
 
@@ -37,38 +44,100 @@ public class GiaodienActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giaodien);
-        setResult(0);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        checkFacebookLogged();
+        setLogoutButton();
         setActionBar();
-//        setDrawerLayout();
+        setDrawerLayout();
         setTablayout();
         setViewPager();
         addEventTab();
-        setLogoutButton();
     }
 
+    // Kiểm tra máy đã login facebook chưa
+    public void checkFacebookLogged() {
+        AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                currentAccessToken = AccessToken.getCurrentAccessToken();
+                if (currentAccessToken == null) {
+                    loginFacebook();
+                }
+            }
+        };
+    }
+
+    // login Facebook
+    public void loginFacebook() {
+        final Dialog d = new Dialog(this);
+        d.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        d.setContentView(R.layout.activity_login_facebook);
+        d.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        d.setCancelable(false);
+        d.show();
+        LoginButton loginButton = (LoginButton) d.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                id = (TextView) findViewById(R.id.txt_giaodien_id);
+                username = (TextView) findViewById(R.id.txt_giaodien_username);
+                Profile profile = Profile.getCurrentProfile();
+                if (profile != null) {
+                    userimg.setImageURI(profile.getProfilePictureUri(50, 50));
+                    id.setText(profile.getId());
+                    username.setText(profile.getName());
+                }
+                d.dismiss();
+            }
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+            }
+        });
+        d.findViewById(R.id.btn_login_exit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                d.dismiss();
+                finish();
+            }
+        });
+    }
+
+    // add sự kiện đăng xuất facebook
+    public void setLogoutButton() {
+        findViewById(R.id.btn_giaodien_logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logOut();
+                drawerLayout.closeDrawer(linearLayout);
+                loginFacebook();
+            }
+        });
+    }
+
+    // set Toolbar
     public void setActionBar() {
         toolbar = (Toolbar) findViewById(R.id.toolbarGiaodien);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Thu chi");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.mipmap.menu_icon);
     }
 
+    // set sự kiện cho drawable và thông tin người dùng
     public void setDrawerLayout() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
         linearLayout = (LinearLayout) findViewById(R.id.layoutGiaodien);
         userimg = (ImageView) findViewById(R.id.img_giaodien_image);
-        id = (TextView) findViewById(R.id.txt_giaodien_id);
-        username = (TextView) findViewById(R.id.txt_giaodien_username);
-        Profile profile = Profile.getCurrentProfile();
-        if (profile != null) {
-            userimg.setImageURI(profile.getProfilePictureUri(50, 50));
-            id.setText(profile.getId());
-            username.setText(profile.getName());
-        }
     }
 
+    // set Taplayout
     public void setTablayout() {
         tablayout = (TabLayout) findViewById(R.id.tablayout);
         tablayout.addTab(tablayout.newTab().setText("Kiếm xu"));
@@ -77,12 +146,14 @@ public class GiaodienActivity extends AppCompatActivity {
         tablayout.setSelectedTabIndicatorHeight(7);
     }
 
+    // set ViewPager
     public void setViewPager() {
         viewPager = (ViewPager) findViewById(R.id.viewpagerGiaodien);
         adapterGiaodich = new AdapterGiaodien(getSupportFragmentManager());
         viewPager.setAdapter(adapterGiaodich);
     }
 
+    // add sụ kiện cho ViewPager và Tablayout
     public void addEventTab() {
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tablayout));
         tablayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -97,17 +168,6 @@ public class GiaodienActivity extends AppCompatActivity {
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
-    }
-
-    public void setLogoutButton() {
-        findViewById(R.id.btn_giaodien_logout).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LoginManager.getInstance().logOut();
-                setResult(1);
-                finish();
             }
         });
     }
